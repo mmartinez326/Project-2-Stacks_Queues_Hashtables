26bea1b0df3eedc1776feaa640498afecae53a96
mmartinez326 <mmartinez3@mail.usf.edu>
2016-06-15 17:48:37 -0400

    Add files via upload

--- Main.cpp
+++ Main.cpp
@@ -3,7 +3,214 @@
 #include "Queue.h"
 #include "Stack.h"
 
+void TestBasicHashTable();
+void TestStack();
+
 int main () {
 
+	//TestBasicHashTable();
+	TestStack();
 
+	cin.get();
+	return 0;
+}
+
+void TestStack () 
+{
+	Stack<char> S;
+
+	cout << "Is Stack empty ?" <<(S.empty()?" YES":" NO") << endl;
+	cout << "Number of elements: " << S.size() << endl;
+	cout << "Current Size: " << S.capacity() << endl;
+	try
+	{
+		cout << "Top data: " << S.top() << endl;
+	}
+	catch (const underflow_error& e)
+	{
+		cerr << e.what() << endl;
+	}
+	S.display();
+	
+	try
+	{
+		cout << endl << "Erased: " << S.erase('M') << endl;
+	}
+	catch (const underflow_error& e)
+	{
+		cerr << e.what() << endl;
+	}
+
+	cout << "--------------------------------------------------------" <<  endl << endl << endl;
+
+	cout << "Adding more than allocated size: " << endl;
+
+	S.Push('M');
+	S.Push('A');
+	S.Push('N');
+	S.Push('U');
+	S.Push('E');
+	S.Push('L');
+	S.Push('M');
+	S.Push('A');
+	S.Push('R');
+	S.Push('T');
+	S.Push('I');
+	S.Push('N');
+	S.Push('E');
+	S.Push('C');
+	S.Push('A');
+	S.Push('R');
+
+	//S.display();
+	//S.clear();
+	//S.display();
+
+
+	cout << "Is Stack empty ?" <<(S.empty()?" YES":" NO") << endl;
+	cout << "Number of elements: " << S.size() << endl;
+	cout << "Current Size: " << S.capacity() << endl;
+	try
+	{
+		cout << "Top data: " << S.top() << endl;
+	}
+	catch (const underflow_error& e)
+	{
+		cerr << e.what() << endl;
+	}
+	S.display();
+	cout << endl << endl << endl;
+
+	try
+	{
+		cout << endl << "Erased: " << S.erase('M') << endl;
+	}
+	catch (const underflow_error& e)
+	{
+		cerr << e.what() << endl;
+	}try
+	{
+		cout << endl << "Erased: " << S.erase('N') << endl;
+	}
+	catch (const underflow_error& e)
+	{
+		cerr << e.what() << endl;
+	}try
+	{
+		cout << endl << "Erased: " << S.erase('A') << endl;
+	}
+	catch (const underflow_error& e)
+	{
+		cerr << e.what() << endl;
+	}
+	S.display();
+	cout << endl;
+
+	cout << "--------------------------------------------------------" <<  endl << endl << endl;
+
+
+	cout << "Using clear() function to resize" << endl;
+	S.clear();
+	S.display();
+
+	cout << "--------------------------------------------------------" <<  endl << endl << endl;
+
+	cout << "adding 'Racecar' : " << endl;
+
+	S.Push('R');
+	S.Push('A');
+	S.Push('C');
+	S.Push('E');
+	S.Push('C');
+	S.Push('A');
+	S.Push('R');
+
+
+	
+	cout << endl << "Is Stack empty ?" <<(S.empty()?" YES":" NO") << endl;
+	cout << "Number of elements: " << S.size() << endl;
+	cout << "Current Size: " << S.capacity() << endl;
+	try
+	{
+		cout << "Top data: " << S.top() << endl;
+	}
+	catch (const underflow_error& e)
+	{
+		cerr << e.what() << endl;
+	}
+	S.display();
+	
+	try
+	{
+		S.pop();
+	}
+	catch (const underflow_error& e)
+	{
+		cerr << e.what() << endl;
+	}
+	try
+	{
+		S.pop();
+	}
+	catch (const underflow_error& e)
+	{
+		cerr << e.what() << endl;
+	}
+	
+
+	cout << endl << endl << endl;
+	cout << "Is Stack empty ?" <<(S.empty()?" YES":" NO") << endl;
+	cout << "Number of elements: " << S.size() << endl;
+	cout << "Current Size: " << S.capacity() << endl;
+	try
+	{
+		cout << "Top data: " << S.top() << endl;
+	}
+	catch (const underflow_error& e)
+	{
+		cerr << e.what() << endl;
+	}
+	S.display();
+
+	S.clear();
+
+	cout << endl << endl << endl;
+	cout << "Is Stack empty ?" <<(S.empty()?" YES":" NO") << endl;
+	cout << "Number of elements: " << S.size() << endl;
+	cout << "Current Size: " << S.capacity() << endl;
+	try
+	{
+		cout << "Top data: " << S.top() << endl;
+	}
+	catch (const underflow_error& e)
+	{
+		cerr << e.what() << endl;
+	}
+	S.display();
+
+	//delete[] S;
+			
+}
+
+void TestBasicHashTable()
+{
+	Hash_Table<int, int> hash_brown;
+
+	hash_brown.insert(29, 29);
+	hash_brown.insert(10, 10);
+	hash_brown.insert(23, 23);
+
+	hash_brown.Display();
+	cout << endl;
+
+	SLinkedList<int> dat = hash_brown.search(10);
+
+	cout << "Key(10).Info:  " << endl;
+	dat.printLst();
+
+	cout << endl;
+
+	cout << "Hash table after delete is called:  " << endl;
+	hash_brown.erase(10);
+	hash_brown.Display();
 }
--- Stack.h
+++ Stack.h
@@ -1,45 +1,232 @@
 #include <iostream>
+#include <cmath>
 
 template <class type> class Stack {
 private:
-	type *data;
-	int count;
-	int initialSize;
-	int arraySize;
+	type *info;
+	int count;					//counts the elements in the array
+	int initialSize;			//initial size of array
+	int arraySize;				//current array size
+	int top_of_array;			//current index for top of array
 
 public:
 
-	Stack(int n=13) {
+	Stack(int n=13) 
+	{
 		if (n<=0) {n=1;}
 		
-		data = new type [n]; 
+		info = new type[n];
 		count = 0;
 		initialSize = n;
-		arraySize = 0;
+		arraySize = n;
+		top_of_array = -1;
+
+		//initialize
+		for (int i=0; i< arraySize; i++) 
+		{
+			this->info[i] = 0;
+		}
 	}
 
-	~Stack() {}
+	~Stack() {delete[] info;}
 
-	type top() const {return 0;}
+	type top() const 
+	{
+		if (top_of_array == -1) {throw underflow_error ("The Stack is empty (Location: top)");}
+		else{return info[top_of_array];}
+	}
 
-	type size() const {return count;}
+	int size() const {return count;}
+	bool empty() const {return (top_of_array == -1);}
+	int capacity() const {return arraySize;} 
 
-	bool empty() const {retrun 0;}
-
-	int capacity() const {return 0;}
-
-	void display() {}
+	void display() 
+	{
+		if (empty()) {cout << "The Stack is empty (Location: display)" << endl;}
+		else {
+			for (int n=top_of_array; n >= 0; n--)
+			{
+				cout << this->info[n];
+			}
+		}
+	}
 
 
 	//Mutators
 
-	void Push (type const &data) {}
+	void Push (type const &data) 
+	{
+		if (count == arraySize) //double size of array
+		{
+			int newSize = 0;
+			newSize = (arraySize * 2);
+			type *tmp;
+			tmp = new type[newSize];
+			copy(info, info + arraySize, tmp);
 
-	type pop() {return 0;}
+			info = new type[newSize];
+			//initialize new values
+			for (int i=0; i< newSize; i++) 
+			{
+				this->info[i] = 0;
+			}
+			copy(tmp, tmp + arraySize, info);
 
-	void clear() {}
+			arraySize = newSize;
+			cout << "Stack size was doubled" << endl;
+		}
+	
+		top_of_array++;
+		info[top_of_array] = data;
+		count++;
+	}
 
-	int erase(type const &data) {return 0;}
+	type pop() 
+	{
+		if (empty()) {throw underflow_error ("The Stack is empty (Location: pop)");}
+		else 
+		{
+		double size_comparison = 0;
+		double c = count;
+		double s = arraySize;
+		size_comparison = c/s;
+
+		if (((size_comparison) <= 0.25) && (arraySize>initialSize)) //Half the array size
+		{
+			int newSize = 0;
+			newSize = (arraySize/2);
+			type *tmp;
+			tmp = new type[newSize];
+			copy(info, info + arraySize, tmp);
+
+			info = new type[newSize];
+			//initialize new values
+			for (int i=0; i< newSize; i++) 
+			{
+				this->info[i] = 0;
+			}
+			copy(tmp, tmp + newSize, info);
+
+			arraySize = newSize;
+			cout << endl << "Stack size was halfed" << endl;
+
+		}
+
+			type tmp = this->top();
+			this->info[top_of_array] = 0;
+			count--;
+			top_of_array--;
+			return tmp;
+		
+		}
+	
+	//If, after the element is removed, the array is 1/4 full and the array size is greater than the initial size, 
+	//the size of the array is halved. This may throw a underflow exception
+	}
+
+	void clear() 
+	{
+		if (empty()) {cout << "The Stack is empty" << endl;}
+		else {
+			for (int n=count; n > 0; n--)
+			{
+				this->info[n] = 0;
+				count--;
+				top_of_array--;
+				//this->pop();
+			}
+			
+			if (arraySize != initialSize)		//Array size is changed to initial size
+			{
+			int newSize = initialSize;
+			type *tmp;
+			tmp = new type[newSize];
+			copy(info, info + arraySize, tmp);
+
+			info = new type[newSize];
+			//initialize new values
+			for (int i=0; i< newSize; i++) 
+			{
+				this->info[i] = 0;
+			}
+			copy(tmp, tmp + newSize, info);
+
+			arraySize = newSize;
+			cout << endl << "Stack size was resized to inital size" << endl;
+			}
+		}
+		
+	}
+
+	
+	//If, after the element(s) is/are removed, the array is 1/4 full and the array size is greater than the initial size, 
+	//the size of the array is halved. Return the number of elements that were deleted. 
+	int erase(type const &data) 
+	{
+		if (empty()) {throw underflow_error ("The Stack is empty (Location: erase)");}
+		else
+		{
+		int numDeleted = 0;
+		int tempIndex = -1;
+		type *tmp;
+		tmp = new type[arraySize];
+
+		for (int n = top_of_array; n>=0; n--)
+			{
+				if (this->info[n] == data)
+				{
+					this->info[n] = 0;
+					count--;
+					top_of_array--;
+					//this->pop();
+					numDeleted++;
+				}
+				else
+				{
+					tempIndex++;
+					tmp[tempIndex] = this->info[n];
+					this->info[n] = 0;
+					count--;
+					top_of_array--;
+					//this->pop();
+				}
+			}
+
+		for (int n=tempIndex; n>=0; n--)
+		{
+			this->Push(tmp[n]);
+		}
+
+
+		//Check if after deleted array needs to be halfed
+		double size_comparison = 0;
+		double c = count;
+		double s = arraySize;
+		size_comparison = c/s;
+		if (((size_comparison) <= 0.25) && (arraySize>initialSize)) //Half the array size
+		{
+			int newSize = 0;
+			newSize = (arraySize/2);
+			type *tmp;
+			tmp = new type[newSize];
+			copy(info, info + arraySize, tmp);
+
+			info = new type[newSize];
+			//initialize new values
+			for (int i=0; i< newSize; i++) 
+			{
+				this->info[i] = 0;
+			}
+			copy(tmp, tmp + newSize, info);
+
+			arraySize = newSize;
+			cout << endl << "Stack size was halfed (erase)" << endl;
+
+		}
+
+		return numDeleted;
+	}
+	}
 
 
 };
